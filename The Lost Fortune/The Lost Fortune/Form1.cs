ddf74b819a3ad9e604a36cb86deca210912eb341
cymback <caspergrozz@gmail.com>
2016-02-02 15:32:24 +0100

    Added WelcomeToGame Form + added startpicture, DO YOU LIKES?

---
+++ The Lost Fortune/The Lost Fortune/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Lost_Fortune
+{
+    public partial class WelomeToGame : Form
+    {
+        public WelomeToGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
