ff199f201d1184bf269f42eb3f9ac637a9e2f500
Nuk3R4z0r <madsmadsen34@hotmail.com>
2016-02-03 17:02:17 +0100

    Linked forms, Started on maps

--- The Lost Fortune/The Lost Fortune/MainForm.cs
+++ The Lost Fortune/The Lost Fortune/MainForm.cs
@@ -12,9 +12,28 @@ namespace The_Lost_Fortune
 {
     public partial class MainForm : Form
     {
+        private MapControl map;
+
         public MainForm()
         {
             InitializeComponent();
+
+            map = new MapControl();
+            map.Visible = false;
+            map.Location = new Point(204, 208);
+            tabControl1.TabPages[0].Controls.Add(map);
+            map.BringToFront();
+        }
+
+        private void btnLand1_Click(object sender, EventArgs e)
+        {
+            map.GenerateMap(1);
+            map.Visible = true;
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
