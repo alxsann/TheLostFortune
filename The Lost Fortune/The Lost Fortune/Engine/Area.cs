7813eb2323084114e544b30b303dfdb3cbafda17
Nuk3R4z0r <madsmadsen34@hotmail.com>
2016-02-06 16:27:12 +0100

    Added Area and Event stubs. Categorized classes in folders

---
+++ The Lost Fortune/The Lost Fortune/Engine/Area.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Lost_Fortune
+{
+    class Area
+    {
+        private string _name;
+        private int _areaId;
+        private List<Entity> _entitiesInArea;
+    }
+}
